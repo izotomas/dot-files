2184fd339400ebbc69ff3975fe651298c69d462b
Tomas Izo <toi@beepanalytics.com>
2020-06-24 22:15:31 +0200

    hammerspoon config

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,66 @@
+-- -----------------------------------------------------------------------
+--           ** HammerSpoon Config File by S1ngS1ng with ❤️ **           --
+-- -----------------------------------------------------------------------
+
+--   ***   Please refer to README.MD for instructions. Cheers!    ***   --
+
+-- -----------------------------------------------------------------------
+--                         ** Something Global **                       --
+-- -----------------------------------------------------------------------
+-- Uncomment this following line if you don't wish to see animations
+-- hs.window.animationDuration = 0
+
+-- -----------------------------------------------------------------------
+--                            ** Requires **                            --
+-- -----------------------------------------------------------------------
+--hhtwm = require('hhtwm')
+require "window-management"
+require "key-binding"
+
+-- -----------------------------------------------------------------------
+--                            ** For Debug **                           --
+-- -----------------------------------------------------------------------
+function reloadConfig(files)
+  local doReload = false
+  for _,file in pairs(files) do
+    if file:sub(-4) == ".lua" then
+      doReload = true
+    end
+  end
+  if doReload then
+    hs.reload()
+    hs.alert.show('Config Reloaded')
+  end
+end
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+
+-- Well, sometimes auto-reload is not working, you know u.u
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "n", function()
+  hs.reload()
+end)
+hs.alert.show("Config loaded")
+
+
+
+hs.hotkey.bind({"ctrl", "alt"}, "tab", function()
+  local screen = hs.mouse.getCurrentScreen()
+  local nextScreen = screen:next()
+  local rect = nextScreen:fullFrame()
+  local center = hs.geometry.rectMidPoint(rect)
+  hs.mouse.setAbsolutePosition(center)
+end)
+
+hs.hotkey.bind({"ctrl", "alt", "shift"}, "tab", function()
+  local screen = hs.mouse.getCurrentScreen()
+  local previousScreen = screen:previous()
+  local rect = previousScreen:fullFrame()
+  local center = hs.geometry.rectMidPoint(rect)
+  hs.mouse.setAbsolutePosition(center)
+end)
+
+hs.hotkey.bind({"ctrl", "alt"}, "down", function()
+  local win = hs.window.focusedWindow()
+  if win then
+    win:moveToScreen(hs.mouse.getCurrentScreen())
+  end
+end)
