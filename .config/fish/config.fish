b0e43188a82a6be06eb13a286b5a60fe5475f12d
Tomas Izo <toizo@dfds.com>
2022-11-03 15:08:01 +0100

    refactor awsenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -114,6 +114,7 @@ function awsenv --argument-names env
     end
 
     set -l AWS_ACCOUNT $(aws sts get-caller-identity | jq -r '.Account')
+    set -l K8S_NAMESPACE $(cat ./k8s/deployment.yml | yq '.metadata.namespace')
 
     # fail if not logged in
     if test -z "$AWS_ACCOUNT"
@@ -124,7 +125,7 @@ function awsenv --argument-names env
     # setup envrc for direnv
     echo "creating .envrc file..."
     printf "export SAML2AWS_ROLE=\"arn:aws:iam::$AWS_ACCOUNT:role/Capability\"
-export K8S_NAMESPACE=\"\$(cat ./k8s/deployment.yml | yq '.metadata.namespace')\"
+export K8S_NAMESPACE=\"$K8S_NAMESPACE\"
 export K8S_ENV=\"$env\"
 ENVRC_AZ=\".envrc.\$K8S_ENV\"
 if test -f \$ENVRC_AZ; then
